7e20e7374d06d761932122ef32a3dfa978022f6a
Ashansa Mahra <mahraashansa1997@gmail.com>
2024-01-03 19:23:27 +0530

    Add files via upload

---
+++ Staircase/Program.cs
@@ -0,0 +1,29 @@
+﻿class Result
+{
+    public static void Staircase(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < n - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.Staircase(n);
+    }
+}
